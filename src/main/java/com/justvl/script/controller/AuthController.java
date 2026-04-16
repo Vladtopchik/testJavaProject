0380667d9fc894d4fba71adfc9d132e6d4de17c8
Vladtopchik <vladkyriniq@gmail.com>
2026-04-16 13:09:35 +0300

    test 9

--- src/main/java/com/justvl/script/controller/AuthController.java
+++ src/main/java/com/justvl/script/controller/AuthController.java
@@ -7,7 +7,6 @@ import com.justvl.script.service.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,7 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("mouseenter", event => {
+    btn.style.backgroundColor = "red";
+});
+
 console.log("hello");
